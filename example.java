44f2d930128440f98f5ba9e96fd3b1d14273788a
cpjunior <jcupid@fandm.edu>
2019-02-25 14:16:57 -0500

    Added writeFile method

--- example.java
+++ example.java
@@ -5,3 +5,11 @@ public static void main(String[] args)
     Random r = new Random();
     System.out.println(r.nextInt());
 }
+
+public void writeFile(int dieNumber){
+
+    File file = new File("Die number");
+    file.write(dieNumber);
+    
+
+}
