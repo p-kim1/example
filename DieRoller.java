600575811b429e1d0d7a898310bca1cd53047f4a
Jordan Cupid <jcupid@fandm.edu>
2019-02-25 23:26:24 -0500

    Fixed writeFile

--- DieRoller.java
+++ DieRoller.java
@@ -1,8 +1,8 @@
-
+import java.io.IOException;
 
 class DieRoller{
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 	Die d6 = new Die(6);
 	int num = d6.roll();
 	example.writeFile(num);
--- example.java
+++ example.java
@@ -1,5 +1,8 @@
 import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Random;
+import java.io.FileWriter;
 
 public class example{
     public static void main(String[] args)
@@ -7,11 +10,13 @@ public class example{
 	Random r = new Random();
 	System.out.println(r.nextInt());
     }
-	public static void writeFile(int dieNumber){
 
+	public static void writeFile(int dieNumber) throws IOException {
 	    File file = new File("Die number");
-	    file.write(dieNumber);
-	    
+		Writer writer = new FileWriter(file);
+	    writer.write(dieNumber + "");
+	    writer.close();
+
 
 	}
 }
